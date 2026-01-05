752dfb101397b6db2199a4ab04df86384431b0f6
David Du <daviddu886@gmail.com>
2026-01-05 14:23:46 -0500

    added logo images

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,13 @@ import './css/App.css'
 import Header from './components/Header'
 import Routes from './components/Routes'
 import Nav from './sections/nav';
-
+import Footer from './sections/footer'
 function App() {
   return (
     <>
       <Nav/>
       <Routes/>
+      <Footer/>
     </>
   )
 }
