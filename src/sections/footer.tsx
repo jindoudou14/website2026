2ca443bafddbda75a7083a3b43ed7a1dcfdf964c
David Du <daviddu886@gmail.com>
2026-01-06 14:36:22 -0500

    readded the removed elements

--- src/sections/footer.tsx
+++ src/sections/footer.tsx
@@ -3,10 +3,12 @@ import "../css/footer.css"
 const Footer = () => {
     return(
         <footer>
-            <a href = "http://127.0.0.1:5500/website2026/index.html"></a>
-            <a href = "https://www.marianopolis.edu/"><img src = "../logos and image/Marianopolis_logo.png"/></a>
-            <a href = "https://robo-crc.ca/">link to crc</a>
-            <a href = "https://github.com/jindoudou14/website2026">link to github repo</a>
+            <div className = "links">
+                <a href = "http://127.0.0.1:5500/website2026/index.html"></a>
+                <a href = "https://www.marianopolis.edu/"><img src = "../logos/Marianopolis_logo.png"/></a>
+                <a href = "https://robo-crc.ca/"><img src = "../logos/CRC_logo.png"/></a>
+                <a href = "https://github.com/jindoudou14/website2026"><img src = "../logos/GitHub_logo.png"/></a>
+            </div>
             <p>Based on the Inside Out Franchise by Pixar Animation Studios </p>
             <p>© 2026 Marianopolis Robotics. All rights reserved.</p>
         
