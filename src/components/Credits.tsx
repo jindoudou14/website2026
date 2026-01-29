ece22ff2fb5019cfdbf11981e1673aa49c72624d
David Du <daviddu886@gmail.com>
2026-01-29 12:44:09 -0500

    credit changes

--- src/components/Credits.tsx
+++ src/components/Credits.tsx
@@ -7,9 +7,9 @@ const Credits = () => {
         <div className = "creds">
             <h1>credits & disclaimer</h1><br></br><br></br>
             <p>This website was created for the CRC Robotics Competition.</p>
-            <p>The design and theme are inspired by the film Inside Out 2 (2024).</p>
+            <p>The design and theme are inspired by the film <em>Inside Out 2 </em> (2024).</p>
             
-            <p>Inside Out 2 and all related characters, images, and trademarks are the property of Disney and Pixar.</p>
+            <p><em>Inside Out 2 </em> and all related characters, images, and trademarks are the property of Disney and Pixar.</p>
             <p>This project is non-commercial and is not affiliated with or endorsed by Disney or Pixar.</p>
             <p></p>
             <p></p>
